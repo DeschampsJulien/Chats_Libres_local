a46bd2f4c37b5ad016761bfb9e1fd489aa05e54d
Julien Deschamps <jdeschamps34@yahoo.fr>
2026-04-25 22:22:48 +0200

    force prod render fix env

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'prod';
-$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? false;
-
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel('prod', false);
 };
-
